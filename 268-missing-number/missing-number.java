218f22aefe7f51032cb988ffa489ca555960c67c
Lingeshwaran <lingeshwaaran85@gmail.com>
2026-09-22 10:16:51 +0530

    Time: 1 ms (28.85%) | Memory: 47.3 MB (48.65%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]<nums.length&&nums[i]!=nums[nums[i]]){
+                swap(nums,i,nums[i]);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index){
+                return index;
+            }
+        }
+        return nums.length;
+    }
+    private void swap(int[]nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
